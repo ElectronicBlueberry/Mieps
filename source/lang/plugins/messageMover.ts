4034c6443ad8d45016592d0b8e27eb10d8decb1b
ElectronicBlueberry <mail@lailalos.com>
2020-05-06 22:30:37 +0200

    added texts

--- source/lang/plugins/messageMover.ts
+++ source/lang/plugins/messageMover.ts
@@ -1,3 +1,35 @@
+import * as Discord from "discord.js";
+
+export const auditLogChannel = `Der Kanal wo der Audit-Log gespeichert werden soll`;
+export const startCutEmoji = `Das Emoji um den Anfang einer Nachrichten Selektion zu makieren`;
+export const endCutEmoji = `Das Emoji um das Ende einer Nachrichten Selektion zu makieren`;
+export const deleteEmoji = `Das Emoji um eine Nachricht zu löschen`;
+export const copyEmoji = `Das Emoji um eine Nachricht zu kopieren`;
+
 export function failMessage(): string {
 	return `Ich konnte leider keine Nachrichten verschieben`;
+}
+
+export function moveMessage(count: number, target: string): string {
+	return `${count} Nachrichten wurden nach ${target} verschoben`;
+}
+
+export function moveSingle(target: string): string {
+	return `Eine Nachricht wurde nach Target verschoben`;
+}
+
+export function logMessage(user: Discord.User): string {
+	return `Gelöscht von: ${user.toString()}`;
+}
+
+export function deleteFailed(): string {
+	return `Löschung Fehlgeschlagen!`;
+}
+
+export function copyLog(user: Discord.User): string {
+	return `Kopiert von ${user.toString()}`;
+}
+
+export function copyFailed(): string {
+	return `Kopieren Fehlgeschlagen!`;
 }
