b246ae081749b4e7a169443da8067d2195384475
ElectronicBlueberry <mail@lailalos.com>
2020-05-07 20:22:55 +0200

    added move command

--- source/lang/plugins/messageMover.ts
+++ source/lang/plugins/messageMover.ts
@@ -1,5 +1,7 @@
 import * as Discord from "discord.js";
 
+import {command_prefix} from "../../config/server.json";
+
 export const pluginDescription = `Erlaubt das verschieben und löschen von Nachrichten`;
 
 export const auditLogChannel = `Der Kanal wo der Audit-Log gespeichert werden soll`;
@@ -10,6 +12,28 @@ export const copyEmoji = `Das Emoji um eine Nachricht zu kopieren`;
 
 export const deleteSingleHelp = `Reagiere mit diesem Emoji auf eine Nachricht um sie zu löschen`;
 export const copySingleHelp = `Reagiere mit diesem Emoji auf eine Nachricht um sie zu dokumentieren`;
+export const startCutHelp = `Reagiere mit diesem Emoji auf eine Nachricht um sie als den Start einer Selektion auszuwählen.
+Dann kannst du folgende Befehle nutzen um etwas mit dieser Selektion zu machen:
+"${command_prefix}move" schneidet die Selektion aus um sie zu verschieben
+"${command_prefix}copy" kopiert die Selektion in einen anderen Kanal
+"${command_prefix}delete" löscht die Selektion`;
+
+export const endCutHelp = `Reagiere mit diesem Emoji auf eine Nachricht um sie als das Ende einer Selektion auszuwählen.
+Dann kannst du folgende Befehle nutzen um etwas mit dieser Selektion zu machen:
+"${command_prefix}move" schneidet die Selektion aus um sie zu verschieben
+"${command_prefix}copy" kopiert die Selektion in einen anderen Kanal
+"${command_prefix}delete" löscht die Selektion`;
+
+export const moveHelp = `Nutze diesen Befehl in einem Kanal, um die selektierten Nachrichten dort hin zu schieben`;
+
+export function moved(count: number, channel: Discord.TextChannel): string {
+	if (count === 1) return `Eine Nachricht wurde nach ${channel} verschoben`;
+	return `${count} Nachrichten wurden nach ${channel} verschoben`;
+}
+
+export function noneSelected(): string {
+	return `Keine selektierten Nachrichten gefunden!`;
+}
 
 export function failMessage(): string {
 	return `Ich konnte leider keine Nachrichten verschieben`;
--- source/plugins/messageMover.plugin.ts
+++ source/plugins/messageMover.plugin.ts
@@ -4,6 +4,10 @@ import { State } from "../modules/state.js";
 
 import * as Discord from "discord.js";
 import { embedFromMessage } from "../modules/embedMaker.js";
+import { stat } from "fs";
+import { stringify } from "querystring";
+
+// ========== Plugin ==========
 
 export default class MessageMover extends Plugin.Plugin implements Plugin.iPlugin {
 	name = "message_mover";
@@ -21,10 +25,14 @@ export default class MessageMover extends Plugin.Plugin implements Plugin.iPlugi
 
 	private deleteSingle = new DeleteSingle(this);
 	private copySingle = new CopySingle(this);
+	private startSelection = new Selection(this, "start");
+	private endSelection = new Selection(this, "end");
 
 	async init(): Promise<void> {
 		this.deleteSingle.emoji = await this.getSetting<Discord.Emoji>("delete_emoji", Plugin.InputType.Emoji);
 		this.copySingle.emoji = await this.getSetting<Discord.Emoji>("copy_emoji", Plugin.InputType.Emoji);
+		this.startSelection.emoji = await this.getSetting<Discord.Emoji>("start_cut_emoji", Plugin.InputType.Emoji);
+		this.endSelection.emoji = await this.getSetting<Discord.Emoji>("end_cut_emoji", Plugin.InputType.Emoji);
 	}
 
 	async getLogChannel(): Promise<Discord.TextChannel | undefined> {
@@ -33,9 +41,70 @@ export default class MessageMover extends Plugin.Plugin implements Plugin.iPlugi
 
 	commands = [
 		this.deleteSingle,
-		this.copySingle
+		this.copySingle,
+		this.startSelection,
+		this.endSelection,
+		new MoveMessages(this)
 	]
 }
+
+// ========== Interfaces ==========
+
+interface SelectionMark {
+	channel: string,
+	message: string,
+	id: string
+}
+
+// ========== Functions ==========
+
+async function fetchSelected(member: Discord.GuildMember, state: State): Promise<Discord.Collection<string, Discord.Message> | undefined> {
+	let startIds: SelectionMark | undefined = state.read(member.id, "start");
+	let endIds: SelectionMark | undefined = state.read(member.id, "end");
+
+	// If no Ids were set, return now
+	if (!startIds || !endIds || startIds.channel !== endIds.channel) return;
+
+	let guild = member.guild;
+	let channel = guild.channels.cache.get(startIds.channel) as Discord.TextChannel;
+
+	// If channel no longer exists, return now
+	if (!channel) return;
+
+	let messages = new Discord.Collection<string, Discord.Message>();
+
+	// This is wrapped in a try catch block, as the messages may be deleted
+	try {
+		// Get the first and the last message
+		let start = await channel.messages.fetch(startIds.message);
+		let end = await channel.messages.fetch(endIds.message);
+		
+		// Start populating the Message Collection
+		messages.set(start.id, start);
+
+		// Keep fetching messages, until we have arrived at the last message
+		let current = start;
+
+		while(current.createdTimestamp < end.createdTimestamp) {
+			let newMessages = await channel.messages.fetch({ after: current.id });
+			newMessages = newMessages.sort((a, b) => {return a.createdTimestamp - b.createdTimestamp});
+			messages = messages.concat(newMessages);
+			current = newMessages.last() as Discord.Message;
+
+			// Edge-Case that occurs when a crucial message was deleted
+			if (!current) break;
+		}
+
+		// Filter all messages, which were psoted after the last message
+		messages = messages.filter(m => m.createdTimestamp <= end.createdTimestamp);
+	} catch {
+		return;
+	}
+
+	return messages;
+}
+
+// ========== Emoji Commands ==========
 
 class DeleteSingle extends Plugin.EmojiCommand {
 	constructor(private plugin: MessageMover) {
@@ -84,6 +153,91 @@ class CopySingle extends Plugin.EmojiCommand {
 			await logChannel?.send(lang.copyLog(member), embed);
 		} catch {
 			logChannel?.send(lang.copyFailed());
+		}
+	}
+}
+
+class Selection extends Plugin.EmojiCommand {
+	constructor(private plugin: MessageMover, private position: string) {
+		super("startSelection");
+	}
+
+	permission = Plugin.Permission.Mod;
+
+	getHelpText() {
+		return lang.startCutHelp;
+	}
+
+	async run(reaction: Discord.MessageReaction, member: Discord.GuildMember): Promise<void> {
+		// Delete the old mark, if one is present
+		let oldR = this.plugin.state.read(member.id, this.position) as SelectionMark | undefined;
+		if (oldR) {
+			try {
+				let guild = member.guild;
+				let channel = guild.channels.cache.get(oldR.channel) as Discord.TextChannel;
+				let oldRMessage = await channel?.messages.fetch(oldR.message);
+
+				if (!(oldRMessage.id === reaction.message.id && oldRMessage.channel.id === reaction.message.channel.id))
+					await (await oldRMessage.reactions.cache.get(oldR.id)?.fetch())?.users.remove(member.id);
+			} catch {}
+		}
+
+		this.plugin.state.write(member.id, this.position, {
+			channel: reaction.message.channel.id,
+			message: reaction.message.id,
+			id: reaction.emoji.id
+		} as SelectionMark);
+	}
+}
+
+// ========== Chat Commands ==========
+
+class MoveMessages extends Plugin.ChatCommand {
+	constructor(private plugin: MessageMover) {
+		super("move");
+	}
+
+	permission = Plugin.Permission.Mod;
+
+	getHelpText() {
+		return lang.moveHelp;
+	}
+
+	async run(message: Discord.Message, args: Array<string>): Promise<void> {
+		let member = message.member as Discord.GuildMember;
+
+		try {
+			let messages = await fetchSelected(member, this.plugin.state);
+
+			if (!messages) {
+				message.channel.send(lang.failMessage());
+				return;
+			}
+
+			let embeds = messages.map(m => {
+				return embedFromMessage(m);
+			});
+
+			// Send Messages to new Channel
+			for (const embed of embeds) {
+				await message.channel.send("", embed);
+			}
+
+			// Delete old Messages
+			messages.forEach(m => {
+				m.delete();
+			});
+
+			// Post Response
+			let attachment = new Discord.MessageAttachment("./img/banner.gif");
+			let sourceChannelId = (this.plugin.state.read(member.id, "start") as SelectionMark).channel;
+			let sourceChannel = member.guild.channels.cache.get(sourceChannelId) as Discord.TextChannel;
+			sourceChannel.send(lang.moved(embeds.length, message.channel as Discord.TextChannel), attachment);
+
+			// Delete Command
+			message.delete();
+		} catch {
+			message.channel.send(lang.failMessage());
 		}
 	}
 }
