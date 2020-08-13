5e01906971de8d522ca3d5e250f473cf18169369
ElectronicBlueberry <mail@lailalos.com>
2020-08-13 22:13:29 +0200

    inital

---
+++ source/lang/plugins/warnings.ts
@@ -0,0 +1,3 @@
+export const warningsDescription = `Erlaubt es Mahnungen zu verfassen, und zu versenden`;
+export const archiveChannelDescription = `Der Kanal in welchem die Mahnungen archivriert werden`;
+export const approveCountDescription = `Wie viele Mods die Mahnung genehmigen müssen, bevor sie versendet werden kann`;
