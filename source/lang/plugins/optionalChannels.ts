dd58d254cfed524432645502b9a4afbe40823330
ElectronicBlueberry <mail@lailalos.com>
2020-08-15 16:42:26 +0200

    change t help text

--- source/lang/plugins/optionalChannels.ts
+++ source/lang/plugins/optionalChannels.ts
@@ -4,10 +4,10 @@ import * as Discord from "discord.js";
 export const optInDescription = `Erlbaut es Optionale Channel einzurichten, welche User selbst joinen können`;
 
 export const optInChannelList = `Eine Liste vom Namen aller opt-In Kanälen, ein Eintrag pro Zeile. Nicht die Kanäle selbst velinken! "-" um leer zu lassen`;
-export const optInRole = `Eine Liste der Rollen für die opt-In Kanäle, in gleicher Reihenfolge wie die Namen. Verlinke die Rollen direkt, mit einem @. "-" um leer zu lassen`;
+export const optInRole = `Eine Liste der Rollen für die opt-In Kanäle, in gleicher Reihenfolge wie die Namen. Verlinke die Rollen direkt, mit einem "@". Schreibe "-" um die Eingabe leer zu lassen`;
 
 export const optOutChannelList = `Eine Liste vom Namen aller opt-Out Kanälen, ein Eintrag pro Zeile. Nicht die Kanäle selbst velinken! "-" um leer zu lassen`;
-export const optOutRole = `Eine Liste der Rollen für die opt-Out Kanäle, in gleicher Reihenfolge wie die Namen. Verlinke die Rollen direkt, mit einem @."-" um leer zu lassen`;
+export const optOutRole = `Eine Liste der Rollen für die opt-Out Kanäle, in gleicher Reihenfolge wie die Namen. Verlinke die Rollen direkt, mit einem "@". Schreibe "-" um die Eingabe leer zu lassen`;
 
 export function cantJoin(channel: string): string {
 	return `Du bist bereits in diesem Channel! Nutze "${command_prefix}leave ${channel}" um ihn zu verlassen`;
